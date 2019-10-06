8870d92ff388165e6d4bbe8c1f43a4be35ffdbc7
Rommel Tinoco <rtinoco@zeppelinops.com>
2019-10-07 02:00:00 +0300

    add docker-javaee image

---
+++ images/docker-javaee/employees/src/main/java/org/javaee/samples/employees/EmployeeEndpoint.java
@@ -0,0 +1,43 @@
+package org.javaee.samples.employees;
+
+import io.prometheus.client.Counter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+
+@Path("employees")
+@ApplicationScoped
+public class EmployeeEndpoint {
+
+    static final Counter requests_get_all = Counter.build()
+            .name("requests_get_all").help("Total GET / requests.").register();
+    static final Counter requests_get_one = Counter.build()
+            .name("requests_get_one").help("Total GET /{id} requests.").register();
+
+    @PersistenceContext
+    EntityManager em;
+    
+    @GET
+    @Produces({"application/xml", "application/json"})
+    public Employee[] get() {
+        requests_get_all.inc();
+        return em.createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0]);
+    }
+    
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Employee get(@PathParam("id") int id) {
+        requests_get_one.inc();
+        return em
+                .createNamedQuery("Employee.findById", Employee.class)
+                .setParameter("id", id)
+                .getSingleResult();
+    }
+}
---
+++ images/docker-javaee/employees/src/main/java/org/javaee/samples/employees/MyApplication.java
@@ -0,0 +1,14 @@
+package org.javaee.samples.employees;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import static org.javaee.samples.employees.MyApplication.APP_ROOT;
+
+
+@ApplicationPath(APP_ROOT)
+public class MyApplication extends Application {
+
+  public static final String APP_ROOT = "/resources";
+    
+}
